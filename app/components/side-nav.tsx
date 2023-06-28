e55614e63d60b9096faeb6eef51afd3d3b11f52e
TheSunSide <60361981+TheSunSide@users.noreply.github.com>
2023-06-27 22:14:17 -0400

    Start, need to confirm why it can be changed only once

--- app/components/side-nav.tsx
+++ app/components/side-nav.tsx
@@ -1,13 +1,15 @@
 import {Sidebar, SidebarProps} from 'flowbite-react'
 import { useState } from 'react';
 import { Routes } from '~/constants/routes'
+import { Theme, useTheme } from '~/utils/theme-provider';
 //import { Theme, useTheme } from '~/utils/theme-provider';
 export default function SideNavPage(content: JSX.Element) {
-  //const [, setTheme] = useTheme();
+  const [, setTheme] = useTheme();
 
-  //const toggleTheme = () => {
-    //setTheme((prevTheme) => (prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
- // };
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
+    console.log("toggleTheme");
+ };
 
   return (
     <main className='flex h-full '>
@@ -58,6 +60,9 @@ export default function SideNavPage(content: JSX.Element) {
                 <input type="checkbox" /*checked={useTheme()[0]==='dark'} onChange={toggleTheme}*/ className="sr-only peer"/>
                 <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
               </label>
+            </li>
+            <li>
+              <button onClick={toggleTheme}>Toggle</button>
             </li>
           </ul>
         </div>
--- app/root.tsx
+++ app/root.tsx
@@ -5,14 +5,32 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
 import clsx from 'clsx';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import tailwindStylesheetUrl from "~/tailwind.css";
 import SideNavPage from "./components/side-nav";
 import React from "react";
-import { CssBaseline, useMediaQuery } from "@mui/material";
+import type { Theme} from "./utils/theme-provider";
+import { NonFlashOfWrongThemeEls, ThemeProvider } from "./utils/theme-provider";
+
+import type { LoaderFunction } from '@remix-run/cloudflare';
+import { getThemeSession } from './utils/theme.server';
+
+export type LoaderData = {
+  theme: Theme | null;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const themeSession = await getThemeSession(request);
+
+  const data: LoaderData = {
+    theme: themeSession.getTheme(),
+  };
+
+  return data;
+};
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -27,11 +45,13 @@ export const meta: MetaFunction = () => ({
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 export function App() {
+  const data = useLoaderData<LoaderData>();
   return (
   <html lang="en" className={"h-full " } >
     <head>
       <Meta />
       <Links />
+      <NonFlashOfWrongThemeEls ssrTheme={Boolean(data.theme)}/>
     </head>
     <body className="h-full bg-slate-50">
       {SideNavPage(<Outlet />)}
@@ -43,31 +63,11 @@ export function App() {
 }
 
 export default function AppWithProviders() {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
+  const data = useLoaderData<LoaderData>();
 
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider specifiedTheme={data.theme}>
+      <App />
+    </ThemeProvider>
   );
 }
