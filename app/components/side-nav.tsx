66e30bdfd9972869887698b27a1ffd4b3fad1014
TheSunSide <60361981+TheSunSide@users.noreply.github.com>
2023-11-10 21:55:20 -0500

    Fixed 'unclickable' button

--- app/components/side-nav.tsx
+++ app/components/side-nav.tsx
@@ -149,7 +149,7 @@ export default function SideNavPage({children}: {children: JSX.Element}) {
 
       </Sidebar>
       <div className='grow w-max h-full'>
-        <button className= {clsx(isCollapsed?"rotate-180 ":"") + "fixed flex m-2 py-2 px-3 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"}  onClick={() => setIsCollapsed(!isCollapsed)}>
+        <button className= {clsx(isCollapsed?"rotate-180 ":"") + "z-10 fixed flex m-2 py-2 px-3 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"}  onClick={() => setIsCollapsed(!isCollapsed)}>
           <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="currentColor" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 476.213 476.213" xmlSpace="preserve">
             <polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5   57.427,253.107 476.213,253.107 "/>
           </svg>
